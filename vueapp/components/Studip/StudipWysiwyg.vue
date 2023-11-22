2ed532deebda85dc58566507a7ffdd0f3c0d21a7
tgloeggl <tgloeggl@uos.de>
2023-11-22 14:54:09 +0100

    fix infotext editing

--- vueapp/components/Studip/StudipWysiwyg.vue
+++ vueapp/components/Studip/StudipWysiwyg.vue
@@ -1,50 +1,70 @@
 <template>
-    <textarea 
+    <textarea
         :value="value"
         ref="studip_wysiwyg"
         class="studip-wysiwyg"
-        @input="updateValue($event.target.value)"/>
+    />
 </template>
 
 <script>
+import { toRaw } from "vue";
+
 // need v-model to provide and get content -> <studip-wysiwyg v-model="content" />
 export default {
     name: 'studip-wysiwyg',
+
     props: {
         value: String
     },
+
     data() {
         return {
-            fallbackActive: false
+            wysiwyg_editor: null,
         }
     },
+
     mounted() {
-        let ckeInit = this.initCKE();
-        if (!ckeInit) {
-            this.fallbackActive = true;
-        }
+        this.initCKE();
     },
+
     methods: {
         initCKE() {
             if (!STUDIP.wysiwyg_enabled) {
                 return false;
             }
-            let view = this;
-            STUDIP.wysiwyg.replace(view.$refs.studip_wysiwyg);
-            let wysiwyg_editor = CKEDITOR.instances[view.$refs.studip_wysiwyg.id];
-            wysiwyg_editor.on('blur', function() {
-                //console.log('cke blur');
-            });
-            wysiwyg_editor.on('change', function() {
-                view.$emit('input', wysiwyg_editor.getData());
-            });
+
+            if (!this.wysiwyg_editor) {
+                this.checkEditor();
+            }
+
             return true;
         },
+
         updateValue(value) {
-            if (this.fallbackActive) {
-                this.$emit('input', value);
+            this.$emit('input', value);
+        },
+
+        checkEditor()
+        {
+            let view = this;
+            let textarea = this.$refs['studip_wysiwyg'];
+
+            if (!STUDIP.wysiwyg.getEditor(textarea)) {
+                STUDIP.wysiwyg.replace(textarea);
+                setTimeout(() => {
+                    view.checkEditor()
+                }, 500);
+                return;
             }
-        }
+
+            this.wysiwyg_editor = STUDIP.wysiwyg.getEditor(textarea);
+
+            // using toRaw to remove Vue proxys. They do not work well with CKEditor
+            toRaw(this.wysiwyg_editor).ui.focusTracker.on( 'change:isFocused', () => {
+                view.updateValue(toRaw(view.wysiwyg_editor).getData());
+            });
+        },
+
     },
 
 }
